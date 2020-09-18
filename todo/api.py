3dfd9c9cdd79c7f9e0136ce07f2cfa7ed871d99e
Nabarun Pal <pal.nabarun95@gmail.com>
2020-09-19 04:46:47 +0530

    Add app level logging machinery
    Signed-off-by: Nabarun Pal <pal.nabarun95@gmail.com>

--- todo/api.py
+++ todo/api.py
@@ -1,24 +1,40 @@
+import logging
 from typing import List, Union
 
 from fastapi import FastAPI, Response, status
 
+from . import config
 from .models import Task, TaskBase, TaskCreate, TaskUpdate
 
 api = FastAPI()
 
+log_level = logging.getLevelName(config.LOG_LEVEL)
+logger = logging.getLogger(__name__)
+logger.setLevel(log_level)
+
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler = logging.StreamHandler()
+handler.setLevel(log_level)
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
+
 
 @api.get("/", response_model=List[Task])
 def list(completed: Union[bool, None] = None) -> List[Task]:
+    logger.info("GET - /")
     return Task.find_all(completed=completed)
 
 
 @api.post("/", response_model=Task, status_code=status.HTTP_201_CREATED)
 def create(task: TaskCreate) -> Task:
+    logger.info("POST - /")
     return Task.create(task.description)
 
 
 @api.get("/{id}", response_model=Task)
 def get(id: int, response: Response) -> Task:
+    logger.info(f"GET - /{id}")
     task = Task.find(id=id)
     if not task:
         response.status_code = status.HTTP_404_NOT_FOUND
@@ -29,6 +45,7 @@ def get(id: int, response: Response) -> Task:
 
 @api.put("/{id}", response_model=Task)
 def update(id: int, _task: TaskUpdate, response: Response) -> Task:
+    logger.info(f"PUT - /{id}")
     task = Task.find(id=id)
     if not task:
         response.status_code = status.HTTP_404_NOT_FOUND
@@ -41,6 +58,7 @@ def update(id: int, _task: TaskUpdate, response: Response) -> Task:
 
 @api.put("/{id}/complete", response_model=Task)
 def mark_complete(id: int, response: Response) -> Task:
+    logger.info(f"PUT - /{id}/complete")
     task = Task.find(id=id)
     if not task:
         response.status_code = status.HTTP_404_NOT_FOUND
@@ -53,6 +71,7 @@ def mark_complete(id: int, response: Response) -> Task:
 
 @api.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete(id: int, response: Response):
+    logger.info(f"DELETE - /{id}")
     task = Task.find(id=id)
     if not task:
         response.status_code = status.HTTP_404_NOT_FOUND
